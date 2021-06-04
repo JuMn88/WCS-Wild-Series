5d6d481446f7d2e9cc4bb4485385822dfcbf5ca2
JuMn88 <moineau.julien@hotmail.fr>
2021-06-04 18:43:03 +0200

    Quest 12

---
+++ src/Form/ProgramType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Program;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+class ProgramType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('category', null, ['choice_label' => 'name'])
+            ->add('poster', TextType::class)
+            ->add('country', CountryType::class)
+            ->add('year', IntegerType::class)
+            ->add('summary', TextareaType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Program::class,
+        ]);
+    }
+}
