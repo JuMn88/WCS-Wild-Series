2606cd8009b1167ca86ebe62386a040840af1f59
Julien <moineau.julien@hotmail.fr>
2022-02-02 18:58:57 +0100

    fix  actor_new

--- src/Form/ActorType.php
+++ src/Form/ActorType.php
@@ -3,17 +3,28 @@
 namespace App\Form;
 
 use App\Entity\Actor;
+use App\Entity\Program;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ActorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('programs')
+            ->add('name', TextType::class, [
+                'label' => 'Nom :'
+            ])
+            ->add('programs', EntityType::class, [
+                'class' => Program::class,
+                'choice_label' => 'title',
+                'label' => 'Série :',
+                'multiple' => true,
+                'expanded' => true,
+            ])
         ;
     }
 
