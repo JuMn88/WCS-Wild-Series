d7eb70ac2553675782bdb0f5614415b1849e0695
Julien <moineau.julien@hotmail.fr>
2022-02-24 18:26:07 +0100

    setting a Category dropdown to the navbar thanks to embedded Controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use App\Repository\ProgramRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
@@ -39,5 +40,12 @@ class DefaultController extends AbstractController
         $urlDestination = preg_replace($pattern, '/' . $locale . '/', $urlOrigin); //replacing the prefix of the url
 
         return $this->redirect($urlDestination);
+    }
+
+    public function navbarTop(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('layout/_navbartop.html.twig', [
+            'categories' => $categoryRepository->findBy([], ['id' => 'DESC'])
+        ]);
     }
 }
